5eb76498342479b58df75077d809b6d6ac788c32
Satish Kr. Yadav <ysatish1988@gmail.com>
2025-07-26 23:49:18 +0530

    Now the react is from npm

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 const jsElement = document.createElement('h1');
 jsElement.innerHTML = 'Hello react from pure JS';
 const jsRoot = document.getElementById('pure-js');
@@ -5,16 +7,16 @@ jsRoot.appendChild(jsElement);
 
 // React code to inject new element in the react js html div
 
-const reactNode = React.createElement('div', {}, [
+const reactNode = React.createElement('div', { id: 'div3', key: 1 }, [
   React.createElement(
     'div',
-    {},
-    React.createElement('h1', { id: 'div1' }, 'hello div 1')
+    { key: 4 },
+    React.createElement('h1', { key: 2 }, 'hello div 1')
   ),
   React.createElement(
     'div',
-    {},
-    React.createElement('h1', { id: 'div2' }, 'hello div 2')
+    { key: 5 },
+    React.createElement('h1', { key: 3 }, 'hello div 2')
   ),
 ]);
 const root = ReactDOM.createRoot(document.getElementById('react-js'));
